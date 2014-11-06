2d651d51d2f663d39f3826d97322728ec8fa9e43
Sunghyun Kang <asdf@ec.cse.cau.ac.kr>
2014-11-06 21:56:00 +0900

    create SentimentAnalyzer

---
+++ src/net/caucse/opinion/SentimentAnalyzer.java
@@ -0,0 +1,86 @@
+package net.caucse.opinion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+
+import net.caucse.opinion.SentiWord.POS;
+import net.caucse.paperlibrary.ScoreMap;
+
+public class SentimentAnalyzer {
+	private ScoreMap<String> wordScore;
+	private SentimentAnalyzer(ScoreMap<String> wordScore) {
+		this.wordScore = wordScore;
+	}
+	
+	public static SentimentAnalyzer load(String filename) throws FileNotFoundException, IOException {
+		SentiWordReader reader = new SentiWordReader(filename);
+		SentimentAnalyzer analyzer = load(reader);
+		reader.close();
+		return analyzer;
+	}
+	
+	public static SentimentAnalyzer load(SentiWordReader reader) throws IOException {
+		SentiWord sentiWord;
+		
+		ScoreMap<String> wordScore = new ScoreMap<String>();
+		
+		while ( (sentiWord = reader.read()) != null) {
+			for (String word : sentiWord.getSynsetTerms()) {
+				if (word.length() < 2) continue;
+				if (word.charAt(0) == '_') continue;
+				if (!word.matches(".*[가-힣].*")) continue;
+				
+				if (sentiWord.getPos() == POS.ADJECTIVE) {
+					int last = word.length() - 1;
+					if (word.charAt(last) == '의' || word.charAt(last) == '한' && word.charAt(last) == '인') {
+						word = word.substring(0, last);
+					} else if (word.charAt(last-1) == '하' && word.charAt(last) == '는') {
+						word = word.substring(0, last-1);
+					} else if (word.charAt(last) == '는') {
+						word = word.substring(0, last);
+					}
+				} else if (sentiWord.getPos() == POS.VERB) {
+					int last = word.length() - 1;
+					if (word.charAt(last-1) == '하' && word.charAt(last) == '다') {
+						word = word.substring(0, last-1);
+					} else if (word.charAt(last) == '다') {
+						word = word.substring(0, last);
+					}
+				}
+				
+				double score = sentiWord.getPosScore() - sentiWord.getNegScore();
+				if (score == 0.0) continue;
+				
+				if (wordScore.containsKey(word)) {
+					double prevScore = wordScore.get(word);
+					wordScore.put(word, (score + prevScore) / 2.0);
+				} else {
+					wordScore.put(word, score);
+				}
+			}
+		}
+		
+		return new SentimentAnalyzer(wordScore);
+	}
+	
+	public double analyze(String[] words) {
+		double score = 0.0;
+		for (String word : words) {
+			if (wordScore.containsKey(word)) {
+				score += wordScore.get(word);
+			}
+		}
+		return score;
+	}
+	
+	public double analyze(Collection<? extends String> words) {
+		double score = 0.0;
+		for (String word : words) {
+			if (wordScore.containsKey(word)) {
+				score += wordScore.get(word);
+			}
+		}
+		return score;
+	}
+}
--- src/net/caucse/opinion/test/SentiWordTest.java
+++ src/net/caucse/opinion/test/SentiWordTest.java
@@ -5,60 +5,16 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.List;
 
-import net.caucse.opinion.SentiWord;
-import net.caucse.opinion.SentiWord.POS;
-import net.caucse.opinion.SentiWordReader;
-import net.caucse.paperlibrary.ScoreMap;
+import net.caucse.opinion.SentimentAnalyzer;
 import net.caucse.paperlibrary.WordList;
 import net.caucse.paperlibrary.WordListReader;
 
 public class SentiWordTest {
 	public static void main(String[] args) {
 		try {
-			SentiWordReader swr = new SentiWordReader("SentiWordNet/SentiWordNet_KR.txt");
-			SentiWord sentiWord;
 			
-			ScoreMap<String> wordScore = new ScoreMap<String>();
-			
+			SentimentAnalyzer analyzer = SentimentAnalyzer.load("SentiWordNet/SentiWordNet_KR.txt");
 			PrintStream ps = new PrintStream("ilbe-opinion-2014.789");
-			
-			while ( (sentiWord = swr.read()) != null) {
-				for (String word : sentiWord.getSynsetTerms()) {
-					if (word.length() < 2) continue;
-					if (word.charAt(0) == '_') continue;
-					if (!word.matches(".*[가-힣].*")) continue;
-					
-					if (sentiWord.getPos() == POS.ADJECTIVE) {
-						int last = word.length() - 1;
-						if (word.charAt(last) == '의' || word.charAt(last) == '한' && word.charAt(last) == '인') {
-							word = word.substring(0, last);
-						} else if (word.charAt(last-1) == '하' && word.charAt(last) == '는') {
-							word = word.substring(0, last-1);
-						} else if (word.charAt(last) == '는') {
-							word = word.substring(0, last);
-						}
-					} else if (sentiWord.getPos() == POS.VERB) {
-						int last = word.length() - 1;
-						if (word.charAt(last-1) == '하' && word.charAt(last) == '다') {
-							word = word.substring(0, last-1);
-						} else if (word.charAt(last) == '다') {
-							word = word.substring(0, last);
-						}
-					}
-					
-					double score = sentiWord.getPosScore() - sentiWord.getNegScore();
-					if (score == 0.0) continue;
-					
-					if (wordScore.containsKey(word)) {
-						double prevScore = wordScore.get(word);
-						wordScore.put(word, (score + prevScore) / 2.0);
-					} else {
-						wordScore.put(word, score);
-					}
-				}
-			}
-			
-			swr.close();
 			
 			WordListReader wlr = new WordListReader("ilbe-words-2014.789"); 
 			WordList list;
@@ -66,11 +22,7 @@ public class SentiWordTest {
 			while ( (list = wlr.read()) != null) {
 				double score = 0.0;
 				for (List<String> line : list) {
-					for (String w : line) {
-						if (wordScore.containsKey(w)) {
-							score += wordScore.get(w);
-						}
-					}
+					score += analyzer.analyze(line);
 				}
 				//System.out.println(++i + ": " + score);
 				ps.println(score);
